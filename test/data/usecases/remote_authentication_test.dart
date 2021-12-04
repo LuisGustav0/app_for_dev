18a5d9dd9e6bcfbd51d48e043b30e87b2ec3ee15
Luis Gustavo <luis.gustav0@live.com>
2021-12-04 12:42:28 -0300

    feat: ensure RemoteAuthentication calls HttpClient with correct Body

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -1,11 +1,14 @@
+import 'package:test/test.dart';
 import 'package:faker/faker.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:test/test.dart';
+
+import 'package:app_for_dev/domain/usecases/usecases.dart';
 
 abstract class HttpClient {
   Future<void>? request({
     required String url,
     required String method,
+    Map body
   });
 }
 
@@ -15,8 +18,17 @@ class RemoteAuthentication {
 
   RemoteAuthentication({required this.httpClient, required this.url});
 
-  Future<void> auth() async {
-    await httpClient.request(url: url, method: 'POST');
+  Future<void> auth(AuthenticationParams params) async {
+    final body = {
+      'email': params.email,
+      'password': params.password
+    };
+
+    await httpClient.request(
+        url: url,
+        method: 'POST',
+        body: body
+    );
   }
 }
 
@@ -41,11 +53,20 @@ main() {
   });
 
   test('Should call HttpClint with correct values', () async {
-    await sut.auth();
+    final params = AuthenticationParams(
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+    );
+
+    await sut.auth(params);
 
     verify(() => httpClient.request(
         url: url,
-        method: 'POST'
+        method: 'POST',
+        body: {
+          'email': params.email,
+          'password': params.password,
+        }
     ));
   });
 }
