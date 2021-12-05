80d801be92d334dd38f97bec3f8a64c8abad3b83
Luis Gustavo <luis.gustav0@live.com>
2021-12-05 14:40:23 -0300

    feat: ensure HttpAdapter throws BadRequestError on 400

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -93,6 +93,14 @@ void main() {
     });
 
     test('Should return BadRequestError if post returns 400', () async {
+      mockPost(400, body: '');
+
+      final future = sut.request(url: url, method: HttpMethod.post);
+
+      expect(future, throwsA(HttpError.badRequest));
+    });
+
+    test('Should return BadRequestError if post returns 400', () async {
       mockPost(400);
 
       final future = sut.request(url: url, method: HttpMethod.post);
